a35ffc34f7e9f7d3a0b8eb62d98881af0d995ea1
Everett Brown <everett_a_brown2@mcpsmd.org>
2024-10-29 07:31:16 -0400

    Initial commit

---
+++ java-swing-app/src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    public static void main(String[] args) { 
+      //add your code here!
+
+
+      
+    }
+}
